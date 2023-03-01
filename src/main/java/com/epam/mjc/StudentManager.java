b7817d6f5cb8a8cca51ada2b105000d429dba7ba
sergruff <sergruff@gmail.com>
2023-03-01 22:01:37 +0100

    S1M2task2

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,28 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find (long studentID) {
+    try {
+      if (Student.getValueOf(studentID) == null) {
+        throw new StudentNotFoundException("Could not find student with ID " + studentID);
+      } else return Student.getValueOf(studentID);
+    } catch (StudentNotFoundException e) {
+      System.err.print(e);
+      throw new IllegalArgumentException("Could not find student with ID " + studentID);
+    }
   }
+
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }
+
+
     }
 
   }
-}
---
+++ src/main/java/com/epam/mjc/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc;
+
+public class StudentNotFoundException extends Exception{
+    public StudentNotFoundException (String message){
+        super(message);
+    }
+}
